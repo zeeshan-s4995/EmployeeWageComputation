2219cba68fe3aafe96e47367ce72103f31402b8c
zeeshan-s4995 <zeeshan.s4995@gmail.com>
2022-05-04 14:19:51 +0530

    Added logic for Employee Presence Check

---
+++ EmpWageComputation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageComputation
+    {
+        bool isPresent;
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        public void IsEmployeePresent()
+        {
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 
 namespace EmployeeWageComputation
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmpWageComputation empObj = new EmpWageComputation();
+            empObj.IsEmployeePresent();
+
         }
     }
 }
